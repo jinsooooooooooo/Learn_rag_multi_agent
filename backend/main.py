4d5a4b0cd2421e8abcb16119bffaaf5c677672b7
Jinsoo-MackbookAir <ljslim27@gmail.com>
2025-11-19 10:56:15 +0900

    refactor(main): 더 이상 지워하지 않는 app_onevnet 대신 lifespan으로 startup, shutdown 이벤트 작업 변환

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,41 @@
 # main.py
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware # CORSMiddleware 임포트
-from routes.health_check import router as health_router
-from routes.chat_routes import router as chat_router
-from routes.meeting_routes import router as meeting_router
-from routes.naver_news_routes import router as naver_news_router
-from routes.news_routes import router as news_router
-from routes.langchain_chat_routes import router as langchain_router
-from routes.langchain_chatstream_routes import router as langchain_stream_router
-from routes.stream_sample_routes import router as stream_sample_router
+from backend.routes.health_check import router as health_router
+from backend.routes.chat_routes import router as chat_router
+from backend.routes.meeting_routes import router as meeting_router
+from backend.routes.naver_news_routes import router as naver_news_router
+from backend.routes.news_routes import router as news_router
+from backend.routes.langchain_chat_routes import router as langchain_router
+from backend.routes.langchain_chatstream_routes import router as langchain_stream_router
+from backend.routes.stream_sample_routes import router as stream_sample_router
 
 
-app = FastAPI(title="RAG Multi-Agent Backend")
+
+# @app.on_event("startup") #on_event(startup / shutdown) 더이상 지원하지 않아 lifespan 으로 변경
+# yield 이전 -> startup: db open
+# yield 이후 -> shttdown: db close
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- yield 이전: 애플리케이션 시작 시 실행될 코드 ---
+    # 서버 시작 시 등록된 모든 라우트를 콘솔에 출력하는 디버그용 코드, 데이터베이스 연결 등
+    print("--- Lifespan: Server is starting up! ---")
+    for route in app.routes:
+        methods = ', '.join(route.methods or [])
+        print(f"  {route.path:30s} → [{methods}]")
+
+
+    yield
+
+    # --- yield 이후 : 애플리케이션 종료 시 실행될 코드 ---
+    # (예: 데이터베이스 연결 해제, 리소스 정리 등)
+    print("--- Lifespan: Server is shutting down! ---")
+
+
+app = FastAPI(title="RAG Multi-Agent Backend",lifespan=lifespan)
+
+
 app.include_router(health_router, prefix="/api")
 app.include_router(chat_router, prefix="/api")
 app.include_router(meeting_router, prefix="/api")
@@ -20,19 +44,6 @@ app.include_router(news_router, prefix="/api")
 app.include_router(langchain_router, prefix="/api")
 app.include_router(langchain_stream_router, prefix="/api")
 app.include_router(stream_sample_router, prefix="/api")
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to RAG Multi-Agent Backend"}
-
-
-@app.on_event("startup")
-def on_startup():
-    # 서버 시작 시 등록된 모든 라우트를 콘솔에 출력하는 디버그용 코드
-    print("\n Registered Routes:")
-    for route in app.routes:
-        methods = ', '.join(route.methods or [])
-        print(f"  {route.path:30s} → [{methods}]")
 
 # 백엔드 (CORS 허용 추가): (React/HTML 등 외부 요청을 허용해야 합니다)
 app.add_middleware(
@@ -43,3 +54,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to RAG Multi-Agent Backend"}
